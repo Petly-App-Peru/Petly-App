8f57b54c54ce0db4aded5a40ed18e2f7576ae8b4
Miguel Collantes <migcol@make.pe>
2024-05-05 20:32:24 -0500

    First push

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect} from 'react';
+import {Provider} from 'react-redux';
+import {components} from './src/components';
+import {persistor, store} from './src/store';
+import {enableScreens} from 'react-native-screens';
+import Orientation from 'react-native-orientation-locker';
+import {PersistGate} from 'redux-persist/integration/react';
+import StackNavigator from './src/navigation/StackNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+enableScreens();
+
+const App = () => {
+  useEffect(() => {
+    Orientation.lockToPortrait();
+  }, []);
+
+  return (
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <PersistGate
+          loading={<components.Loader />}
+          persistor={persistor}
+        >
+          <NavigationContainer>
+            <StackNavigator />
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
+      <components.FlashMessage />
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
